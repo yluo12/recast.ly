bf9273767d156b23d8d8cfbbaadf95983926d001
yluo12 <yantingluo98@gmail.com>
2023-04-10 21:50:17 -0700

    updated App.jsx, VideoList.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,36 @@
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
-      </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
-      </div>
-      <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+import exampleVideoData from '../data/exampleVideoData.js';
+import VideoList from './VideoList.js';
+
+const {useState} = React;
+var App = () => {
+  const [videoList, setVideoList] = useState(exampleVideoData);
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="col-md-6 offset-md-3">
+          <div><h5><em>search</em> view goes here</h5></div>
+        </div>
+      </nav>
+
+      <div className="row">
+        <div className="col-md-7">
+          <div><h5><em>videoPlayer</em> view goes here</h5></div>
+        </div>
+
+
+
+        <VideoList videoList={videoList} />
       </div>
     </div>
-  </div>
-);
+  );
+};
+
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 export default App;
+
+
+
+
+
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,10 +1,17 @@
-var VideoList = () => (
+import VideoListEntry from './VideoListEntry.js';
+
+var VideoList = ({videoList}) => (
+  // <div className="video-list">
+  //   <div><h5><em>videoListEntry</em> view goes here</h5></div>
+  //   <div><h5><em>videoListEntry</em> view goes here</h5></div>
+  //   <div><h5><em>videoListEntry</em> view goes here</h5></div>
+  //   <div><h5><em>videoListEntry</em> view goes here</h5></div>
+  //   <div><h5><em>videoListEntry</em> view goes here</h5></div>
+  // </div>
+
+
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    {videoList.map((video) => <VideoListEntry video={video} key={video.id.videoId} />)}
   </div>
 );
 
